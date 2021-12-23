ea6c4c402e351e4c091502b4111524110e21e999
MaicolAvila <maicol.2001@hotmail.com>
2021-12-23 11:45:31 -0500

    Edit ready!

--- src/pages/Edit/Edit.tsx
+++ src/pages/Edit/Edit.tsx
@@ -4,7 +4,7 @@ import {
   useState,
   useEffect,
 } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./Edit.scss";
 import { useSetRecoilState, useRecoilValue, useRecoilState } from "recoil";
 
@@ -31,13 +31,33 @@ export default function Edit(props: any) {
     const dev = developers.find((dev) => dev.id === props.match.params.id);
 
     if (dev) setContent(dev);
+    else history.replace("/");
   }, []);
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setContent((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
+  const handleChangeDes: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setContent((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const history = useHistory();
+
   const setDeveloper = useSetRecoilState(productContentState);
+
+  const deleteProduct = async () => {
+    try {
+      const colect = await db.collection("products").get();
+      const product = colect.docs.find(
+        (doc) => doc.data().id === props.match.params.id
+      );
+      if (product) db.collection("products").doc(product.id).delete();
+      history.replace("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const editDeveloper: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -56,6 +76,7 @@ export default function Edit(props: any) {
       setTimeout(() => {
         setSuccess(false);
       }, 2000);
+      history.replace("/");
     } else {
       setError(true);
       setTimeout(() => {
@@ -65,48 +86,48 @@ export default function Edit(props: any) {
   };
 
   return (
-    <form className="Create column" onSubmit={editDeveloper}>
-      <div className="header">
-        <div className="title">Actualizar Desarrollador</div>
-      </div>
-      <div className="column body">
-        <div className="input-div">
-          <label className="title">Nombre</label>
-          <input
-            className="input"
-            placeholder="Ejemplo Maicol..."
-            onChange={handleChange}
-            value={content.name}
-            id="name"
-          />
+    <form className="Edit column" onSubmit={editDeveloper}>
+      <div className="Edit-content row">
+        <div className="header c1 column">
+          <div className="input-div">
+            <input
+              className="input title"
+              placeholder=""
+              onChange={handleChange}
+              value={content.name}
+              id="name"
+            />
+          </div>
+          <div className="content-image">
+            <img src={content.images} style={{ width: "100%" }} alt="" />
+          </div>
         </div>
-        <div className="input-div">
-          <label className="title">Descripcion</label>
-          <input
-            id="description"
-            className="input"
-            onChange={handleChange}
-            value={content.description}
-          />
-        </div>
-        <div className="input-div">
-          <label className="title">Precio</label>
-          <input
-            id="precio"
-            className="input"
-            onChange={handleChange}
-            value={content.precio}
-            type="number"
-          />
+        <div className="body c2 column">
+          <div className="input-div">
+            <input
+              id="precio"
+              className="input precio"
+              onChange={handleChange}
+              value={content.precio}
+            />
+          </div>
+          <div className="input-div">
+            <textarea
+              id="description"
+              className="input textTarea"
+              onChange={handleChangeDes}
+              value={content.description}
+            />
+          </div>
         </div>
       </div>
       <div className="footer row">
         <button className="btn1" type="submit">
-          Editar
+          Actualizar
         </button>
-        <Link to="/" className="btn2">
-          Ver
-        </Link>
+        <button onClick={deleteProduct} className="btn2">
+          Eliminar
+        </button>
       </div>
       {error ? <WarningMessage title="Faltan datos" type="error" /> : null}
       {success ? (
